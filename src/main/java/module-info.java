61549c2fb92dc30dfd039e21688856fb07613cd6
qiushui <qiushui@zgpgc.com>
2019-02-19 14:27:47 +0800

    java 11

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author qiushui on 2019-02-18.
+ */
+module develop.toolkit {
+
+    requires lombok;
+
+    exports develop.toolkit.exception;
+    exports develop.toolkit.struct;
+    exports develop.toolkit.utils;
+}
