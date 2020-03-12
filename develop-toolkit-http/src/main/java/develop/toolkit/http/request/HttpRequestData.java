6bddbd0fad16e9bac5482fdd3cb09989417df2c6
qiushui <qiushui@zgpgc.com>
2020-03-12 08:53:55 +0800

    开发

--- develop-toolkit-http/src/main/java/develop/toolkit/http/request/HttpRequestData.java
+++ develop-toolkit-http/src/main/java/develop/toolkit/http/request/HttpRequestData.java
@@ -32,6 +32,7 @@ public class HttpRequestData {
     @Setter
     private Charset charset = StandardCharsets.UTF_8;
 
+    @Setter
     private HttpRequestDataBody body;
 
     public HttpRequestData(HttpMethod httpMethod, String url) {
