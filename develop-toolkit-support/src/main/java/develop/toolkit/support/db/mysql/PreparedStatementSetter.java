7d5f983e66c0cb70501467af7f543d49f45e6fa4
qiushui <qiushui@zgpgc.com>
2020-05-03 17:17:29 +0800

    开发

--- develop-toolkit-support/src/main/java/develop/toolkit/support/db/mysql/PreparedStatementSetter.java
+++ develop-toolkit-support/src/main/java/develop/toolkit/support/db/mysql/PreparedStatementSetter.java
@@ -1,11 +1,12 @@
 package develop.toolkit.support.db.mysql;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * @author qiushui on 2019-09-03.
  */
 public interface PreparedStatementSetter {
 
-    void set(PreparedStatement preparedStatement);
+    void set(PreparedStatement preparedStatement) throws SQLException;
 }
