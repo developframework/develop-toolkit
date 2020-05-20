559c7fc7d979625dea3ae7e113b8277f36c14083
qiushui <qiushui@zgpgc.com>
2020-05-20 16:23:47 +0800

    优化项目

---
+++ develop-toolkit-db/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * @author qiushui on 2019-02-27.
+ */
+module develop.toolkit.db {
+    requires develop.toolkit.base;
+    requires expression;
+    requires java.net.http;
+    requires java.sql;
+    requires lombok;
+    requires mysql.connector.java;
+    requires org.apache.commons.lang3;
+
+    exports develop.toolkit.db;
+    exports develop.toolkit.db.mysql;
+}
--- develop-toolkit-http/src/main/java/develop/toolkit/http/request/HttpMethod.java
+++
@@ -1,9 +0,0 @@
-package develop.toolkit.http.request;
-
-/**
- * @author qiuzhenhao
- */
-public enum HttpMethod {
-
-    GET, POST, PUT, DELETE
-}
