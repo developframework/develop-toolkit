f2de184775cc670cd5f1ec3e1353b3af1a11c57c
qiushui <qiushui@zgpgc.com>
2020-06-28 14:43:14 +0800

    车牌验证

--- develop-toolkit-world/src/main/java/develop/toolkit/world/verify/CarPlateVerify.java
+++ develop-toolkit-world/src/main/java/develop/toolkit/world/verify/CarPlateVerify.java
@@ -7,10 +7,6 @@ package develop.toolkit.world.verify;
  */
 public final class CarPlateVerify {
 
-    private static final String PROVINCES = "京津晋冀蒙辽吉黑沪苏浙皖闽赣鲁豫鄂湘粤桂琼渝川贵云藏陕甘青宁新";
-
-    private static final String PLATE_CHARS = "ABCDEFGHJKLMNPQRSTUVWXYZ";
-
     private static final String PLATE_CHARS_NUMBER = "0123456789";
 
     private static final String PLATE_CHARS_OTHER = "学挂";
@@ -22,19 +18,56 @@ public final class CarPlateVerify {
      * @return
      */
     public static boolean checkValid(String plate) {
+        final String PROVINCES = "京津晋冀蒙辽吉黑沪苏浙皖闽赣鲁豫鄂湘粤桂琼渝川贵云藏陕甘青宁新";
         String provinceShortName = String.valueOf(plate.charAt(0));
         if (!PROVINCES.contains(provinceShortName)) {
             return false;
         }
         if (plate.length() == 7) {
-            for (int i = 1; i < 7; i++) {
-                if (!PLATE_CHARS.contains(String.valueOf(plate.charAt(i)))) {
-                    return false;
+            return validNormal(plate);
+        } else if (plate.length() == 8) {
+            return validNewEnergy(plate);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 普通民用车牌
+     */
+    private static boolean validNormal(String plate) {
+        final String PLATE_CHARS_ORG = "ABCDEFGHJKLMNPQRSTUVWXY";
+        final String PLATE_CHARS = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789";
+        if (PLATE_CHARS_ORG.contains(String.valueOf(plate.charAt(1)))) {
+            for (int i = 2; i < 7; i++) {
+                String ch = String.valueOf(plate.charAt(i));
+                if (!PLATE_CHARS.contains(ch)) {
+                    return i == 6 && "学挂".contains(ch);
                 }
             }
-        } else if (plate.length() == 8) {
-
+            return true;
         }
-        return true;
+        return false;
+    }
+
+    /**
+     * 新能源车牌
+     */
+    private static boolean validNewEnergy(String plate) {
+        final String PLATE_CHARS_ORG = "ABCDEFGHJKLMNPQRSTUVWXY";
+        if (PLATE_CHARS_ORG.contains(String.valueOf(plate.charAt(1)))) {
+            if (
+                    "123456789DF".contains(String.valueOf(plate.charAt(2))) &&
+                            "ABCDEFGHJKLMNPQRSTUVWXYZ123456789".contains(String.valueOf(plate.charAt(3))) &&
+                            "123456789DF".contains(String.valueOf(plate.charAt(7)))
+            ) {
+                for (int i = 4; i < 6; i++) {
+                    if ("0123456789".contains(String.valueOf(plate.charAt(i)))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
