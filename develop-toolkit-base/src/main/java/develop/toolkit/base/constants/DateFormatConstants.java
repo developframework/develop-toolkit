f22498e44c7224a8c7f2c2164d6b67f1c1fd01ec
qiushui <qiushui@zgpgc.com>
2020-07-17 11:18:27 +0800

    开发

--- develop-toolkit-base/src/main/java/develop/toolkit/base/constants/DateFormatConstants.java
+++ develop-toolkit-base/src/main/java/develop/toolkit/base/constants/DateFormatConstants.java
@@ -14,9 +14,12 @@ public interface DateFormatConstants {
     String COMPACT = "yyyyMMddHHmmss";
     String DATE = "yyyy-MM-dd";
     String TIME = "HH:mm:ss";
-    String MYSQL_FORMAT_DATETIME = "%Y-%m-%d %H:%i:%s";
+    String MYSQL_FORMAT_DATETIME = "%Y-%m-%d %H:%i:%S";
     String MYSQL_FORMAT_DATE = "%Y-%m-%d";
     String MYSQL_FORMAT_TIME = "%H:%i:%s";
+    String MYSQL_FORMAT_MONTH = "%Y-%m";
 
     DateTimeFormatter STANDARD_FORMATTER = DateTimeFormatter.ofPattern(STANDARD);
+    DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE);
+    DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME);
 }
