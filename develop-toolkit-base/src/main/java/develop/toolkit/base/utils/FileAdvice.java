f2e251107cc3d17c47a4f02677d4c24b1e48645f
qiushui <qiushui@zgpgc.com>
2021-03-16 17:55:08 +0800

    开发

--- develop-toolkit-base/src/main/java/develop/toolkit/base/utils/FileAdvice.java
+++ develop-toolkit-base/src/main/java/develop/toolkit/base/utils/FileAdvice.java
@@ -1,30 +1,78 @@
 package develop.toolkit.base.utils;
 
-import java.io.File;
-import java.io.FileFilter;
+import lombok.SneakyThrows;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
+import java.util.function.Predicate;
 
 /**
  * @author qiushui on 2021-03-15.
  */
 public abstract class FileAdvice {
 
-    public static List<File> files(File dir, FileFilter filter) {
+    /**
+     * 打开缓冲写
+     */
+    public static BufferedWriter open(File file, boolean append) throws IOException {
+        return new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(file, append),
+                        StandardCharsets.UTF_8
+                )
+        );
+    }
+
+    @SneakyThrows(IOException.class)
+    public static void write(File file, String text, boolean append) {
+        if (file.exists() || file.getParentFile().mkdirs()) {
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file, append), StandardCharsets.UTF_8)) {
+                writer.write(text);
+            }
+        }
+    }
+
+    @SneakyThrows(IOException.class)
+    public static void write(File file, List<String> lines, boolean append) {
+        if (file.exists() || file.getParentFile().mkdirs()) {
+            try (BufferedWriter writer = open(file, append)) {
+                for (String line : lines) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+        }
+    }
+
+    /**
+     * 遍历目录 找到所有满足条件的文件
+     */
+    public static List<File> files(File dir, Predicate<File> predicate) {
         if (dir.isFile()) {
             return Collections.emptyList();
         }
         List<File> files = new LinkedList<>();
-        recursiveFiles(dir, filter, files);
-        return Collections.unmodifiableList(files);
+        Stack<File> stack = new Stack<>();
+        stack.push(dir);
+        do {
+            eachFiles(stack.pop(), stack, files, predicate);
+        } while (!stack.empty());
+        return List.copyOf(files);
     }
 
-    private static void recursiveFiles(File dir, FileFilter filter, List<File> files) {
-        if (dir.isDirectory()) {
-            final File[] subFiles = dir.listFiles(filter);
-            if (subFiles != null && subFiles.length > 0) {
-                files.addAll(List.of(subFiles));
+    private static void eachFiles(File dir, Stack<File> stack, List<File> files, Predicate<File> predicate) {
+        final File[] listFiles = dir.listFiles();
+        if (listFiles != null) {
+            for (File file : listFiles) {
+                if (file.isDirectory()) {
+                    stack.push(file);
+                } else if (predicate.test(file)) {
+                    files.add(file);
+                }
             }
         }
     }
