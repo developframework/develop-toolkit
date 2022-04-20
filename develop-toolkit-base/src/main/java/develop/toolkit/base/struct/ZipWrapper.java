85692f9c368fd7879932528ac646152a02136ca0
developframework <408000511@qq.com>
2022-04-20 22:21:01 +0800

    zip

--- develop-toolkit-base/src/main/java/develop/toolkit/base/struct/ZipWrapper.java
+++ develop-toolkit-base/src/main/java/develop/toolkit/base/struct/ZipWrapper.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.InputStream;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.zip.ZipEntry;
 
 /**
  * @author qiushui on 2022-04-19.
@@ -21,4 +24,40 @@ public class ZipWrapper {
     private boolean file;
 
     private List<ZipWrapper> children;
+
+    private boolean stored;
+
+    private long compressedSize;
+
+    private long size;
+
+    private long crc;
+
+    private FileTime creationTime;
+
+    private FileTime lastAccessTime;
+
+    private FileTime lastModifyTime;
+
+    private long time;
+
+    private String comment;
+
+    private LocalDateTime timeLocal;
+
+    private byte[] extra;
+
+    public void configureZipEntry(ZipEntry zipEntry) {
+        zipEntry.setMethod(stored ? ZipEntry.STORED : ZipEntry.DEFLATED);
+        zipEntry.setCompressedSize(compressedSize);
+        zipEntry.setSize(size);
+        zipEntry.setCrc(crc);
+        zipEntry.setCreationTime(creationTime);
+        zipEntry.setLastAccessTime(lastAccessTime);
+        zipEntry.setLastModifiedTime(lastModifyTime);
+        zipEntry.setTime(time);
+        zipEntry.setTimeLocal(timeLocal);
+        zipEntry.setExtra(extra);
+        zipEntry.setComment(comment);
+    }
 }
--- develop-toolkit-base/src/main/java/develop/toolkit/base/utils/CompressAdvice.java
+++ develop-toolkit-base/src/main/java/develop/toolkit/base/utils/CompressAdvice.java
@@ -3,6 +3,13 @@ package develop.toolkit.base.utils;
 import develop.toolkit.base.struct.ZipWrapper;
 
 import java.io.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.List;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
@@ -72,22 +79,80 @@ public abstract class CompressAdvice {
 
     public static class Zip {
 
-        public static void compress(ZipWrapper zipWrapper, OutputStream outputStream) {
-            ZipOutputStream zos = new ZipOutputStream(outputStream);
+        public static void compress(Path path, OutputStream outputStream) throws IOException {
+            try (ZipOutputStream zos = new ZipOutputStream(outputStream)) {
+                Files.walkFileTree(path, new FileVisitor<>() {
+
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFile(Path filePath, BasicFileAttributes attrs) throws IOException {
+                        String currentName = filePath.subpath(path.getNameCount(), filePath.getNameCount()).toString();
+                        File file = filePath.toFile();
+                        final ZipEntry zipEntry = new ZipEntry(currentName);
+                        zipEntry.setMethod(ZipEntry.DEFLATED);
+                        zipEntry.setLastModifiedTime(FileTime.fromMillis(file.lastModified()));
+                        zos.putNextEntry(zipEntry);
+                        try (InputStream is = new FileInputStream(file)) {
+                            is.transferTo(zos);
+                        }
+                        zos.closeEntry();
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            }
+        }
+
+        public static byte[] compress(Path path) throws IOException {
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                compress(path, baos);
+                return baos.toByteArray();
+            }
+        }
+
+        public static void compress(Path path, Path outPath) throws IOException {
+            try (OutputStream os = new FileOutputStream(outPath.toFile())) {
+                compress(path, os);
+            }
+        }
 
 
+        public static void compress(ZipWrapper zipWrapper, OutputStream outputStream) throws IOException {
+            try (ZipOutputStream zos = new ZipOutputStream(outputStream)) {
+                recursiveCompress(zipWrapper, "", zos);
+            }
         }
 
         private static void recursiveCompress(ZipWrapper zipWrapper, String parentPath, ZipOutputStream zos) throws IOException {
+            final String currentName = parentPath + zipWrapper.getFilename();
             if (zipWrapper.isFile()) {
-                final ZipEntry zipEntry = new ZipEntry(parentPath + File.separator + zipWrapper.getFilename());
+                final ZipEntry zipEntry = new ZipEntry(currentName);
+                zipWrapper.configureZipEntry(zipEntry);
                 zos.putNextEntry(zipEntry);
                 try (InputStream is = zipWrapper.getInputStreamSupplier().get()) {
                     is.transferTo(zos);
                 }
                 zos.closeEntry();
             } else {
-
+                List<ZipWrapper> children = zipWrapper.getChildren();
+                if (children != null) {
+                    for (ZipWrapper childZipWrapper : children) {
+                        recursiveCompress(childZipWrapper, currentName + File.separator, zos);
+                    }
+                }
             }
         }
     }
